5b5e9251a915949071c9fb0201bce48c05af9876
Christian Pschorr <pschorr.christian@gmail.com>
2024-05-06 22:52:14 +0200

    Umbenennung Modular in Kist Values

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 
 return [
-	'ps14-module-modulor' => [
+	'ps14-module-kist-values' => [
 		'provider' => SvgIconProvider::class,
-		'source' => 'EXT:ps14_modulor/Resources/Public/Icons/ps14-module-modulor.svg',
+		'source' => 'EXT:ps14_kist_values/Resources/Public/Icons/ps14-module-kist-values.svg',
 	],
 ];
