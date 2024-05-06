6f0186d83cb2fb70e25b9d4c98e39bc0cb87c022
Christian Pschorr <pschorr.christian@gmail.com>
2024-05-06 22:48:42 +0200

    Initialer Commit nach Kopie der Modular Extension

---
+++ ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+	'title' => 'Ps14 Kist Values',
+	'description' => 'A TYPO3 extension for displaying company values from KIST + ESCHERICH',
+	'category' => 'distribution',
+	'author' => 'Christian Pschorr',
+	'author_email' => 'pschorr.christian@gmail.com',
+	'author_company' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'clearCacheOnLoad' => 0,
+	'version' => '1.0.0',
+	'constraints' => [
+		'depends' => [
+			'typo3' => '12.0.0-12.4.99',
+			'ps14_foundation' => '2.0.0-2.9.99',
+		],
+		'conflicts' => [],
+		'suggests' => [],
+	],
+];
